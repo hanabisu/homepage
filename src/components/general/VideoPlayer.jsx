7bf6d914ff4a8c8e627a4fd21ee265f6a4ec8fdc
gabouryn <ngaboury@gmail.com>
2022-06-20 13:25:34 -0400

    fix incorrect id props and typo in description

--- src/components/general/VideoPlayer.jsx
+++ src/components/general/VideoPlayer.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function VideoPlayer(id) {
+function VideoPlayer({ id }) {
   return (
-    <div className="video-responsive">
+    <div className="video-frame">
       <iframe width="100%" height="345" title="Embedded youtube" allowFullScreen src={`https://www.youtube.com/embed/${id}`} />
     </div>
   );
 }
+
+VideoPlayer.propTypes = {
+  id: PropTypes.string.isRequired,
+};
 
 export default VideoPlayer;
